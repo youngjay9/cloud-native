628817a180b92520a2bb31dbeef3a2d68885415c
JayHung <youngjay9@gmail.com>
2023-06-25 10:32:52 +0800

    測試 service discovery.

--- category-service/src/main/java/com/jay/controller/CatagoryController.java
+++ category-service/src/main/java/com/jay/controller/CatagoryController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin
 @RestController
@@ -13,5 +14,15 @@ public class CatagoryController {
   public String getCategory(HttpServletRequest request) {
 
     return "Category Something ...";
+  }
+
+  @RequestMapping(value = "/invokeTempService", produces = "application/json")
+  public String invokeTempService(HttpServletRequest request) {
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    String result = restTemplate.getForObject("http://temp-service.study-ingress.svc.cluster.local/temp-api/getPath", String.class);
+
+    return result;
   }
 }
