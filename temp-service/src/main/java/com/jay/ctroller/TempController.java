b3365e6d40df1e86af31d26454bfc881412c4bb8
JayHung <youngjay9@gmail.com>
2023-06-25 11:07:55 +0800

    no message

--- temp-service/src/main/java/com/jay/ctroller/TempController.java
+++ temp-service/src/main/java/com/jay/ctroller/TempController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin
 @RestController
@@ -13,5 +14,18 @@ public class TempController {
   public String getURLValue(HttpServletRequest request) {
     String test = request.getRequestURI();
     return test;
+  }
+
+  @RequestMapping(value = "/invokeCategoryService", produces = "application/json")
+  public String invokeCategoryService(HttpServletRequest request) {
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    String result = restTemplate
+        .getForObject(
+            "http://category-service.study-ingress.svc.cluster.local/category/getCategory",
+            String.class);
+
+    return result;
   }
 }
